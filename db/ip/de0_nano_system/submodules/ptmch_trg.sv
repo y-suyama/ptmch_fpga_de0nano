d348b5dc51197415178304fefaf0ea1e48a06a35
y-suyama <suyama.yasuharu@gmail.com>
2024-07-31 23:11:42 +0900

    step5 commit

--- db/ip/de0_nano_system/submodules/ptmch_trg.sv
+++ db/ip/de0_nano_system/submodules/ptmch_trg.sv
@@ -49,10 +49,7 @@ module ptmch_trg(
 //=================================================================
     logic [31: 0]  sr_inst_sht;
     logic [ 5: 0]  sr_inst_cnt;
-    logic [31: 0]  ar_inst_chk;
-    logic [31: 0]  sr_inst_chk_1d;
-    logic [31: 0]  sr_inst_chk_2d;
-    logic [31: 0]  sr_inst_chk_3d;
+    logic [31: 0]  sr_inst_chk;
     logic          c_inst_mch;
     logic          ar_inst_mch_sft1;
     logic          sr_inst_mch_sft2;
@@ -65,35 +62,61 @@ module ptmch_trg(
     logic          sr_cs_sync_sft2;
     logic          c_cs_edge;
     logic          n_trg_pls;
+    
+    //program execute match
+    logic          c_prgex_ins;
+    logic          c_prgex_addr_l;
+    logic          c_prgex_addr_h;
+    logic          c_prgex_mch;
+    //read status match
+    logic          c_rdsts_ins;
+    logic          c_rdsts_addr_l;
+    logic          c_rdsts_addr_h;
+    logic          c_rdsts_mch;
+    //128KB Block Erase match
+    logic          c_128kbbe_ins;
+    logic          c_128kbbe_addr_l;
+    logic          c_128kbbe_addr_h;
+    logic          c_128kbbe_mch;
+    //page data read match
+    logic          c_pagedread_ins;
+    logic          c_pagedread_addr_l;
+    logic          c_pagedread_addr_h;
+    logic          c_pagedread_mch;
+    //write status match
+    logic          c_wrsts_ins;
+    logic          c_wrsts_addr_l;
+    logic          c_wrsts_addr_h;
+    logic          c_wrsts_mch;
+
+    logic          c_stopcnt;
+    logic          c_loadcnt;
 //=================================================================
 //  output Port
 //=================================================================
     // TRG_PLS Output sel
-    assign   TRG_PLS[0]  = (sr_inst_chk_3d[31:24] == p_program_excute )? n_trg_pls:
-                                                                         1'b0;
-    assign   TRG_PLS[1]  = (sr_inst_chk_3d[31:24] == p_readstatus1 | sr_inst_chk_3d[31:24] == p_readstatus2)? n_trg_pls:
+    assign   TRG_PLS[0]  = (sr_inst_chk[31:24] == p_program_excute )? n_trg_pls:
+                                                                      1'b0;
+    assign   TRG_PLS[1]  = (sr_inst_chk[31:24] == p_readstatus1 | sr_inst_chk[31:24] == p_readstatus2)? n_trg_pls:
                                                                                                               1'b0;
-    assign   TRG_PLS[2]  = (sr_inst_chk_3d[31:24] == p_128kb_blockerase )? n_trg_pls:
+    assign   TRG_PLS[2]  = (sr_inst_chk[31:24] == p_128kb_blockerase )? n_trg_pls:
                                                                            1'b0;
-    assign   TRG_PLS[3]  = (sr_inst_chk_3d[31:24] == p_pagedata_read )? n_trg_pls:
+    assign   TRG_PLS[3]  = (sr_inst_chk[31:24] == p_pagedata_read )? n_trg_pls:
                                                                         1'b0;
-    assign   TRG_PLS[4]  = (sr_inst_chk_3d[31:24] == p_writestatus1 | sr_inst_chk_3d[31:24] == p_writestatus2)? n_trg_pls:
+    assign   TRG_PLS[4]  = (sr_inst_chk[31:24] == p_writestatus1 | sr_inst_chk[31:24] == p_writestatus2)? n_trg_pls:
                                                                                                                 1'b0;
     assign   c_inst_edge = (c_inst_mch & ~sr_inst_mch_sft2);
-//    assign   c_cs_edge = (sr_cs_sync & ~sr_cs_sync_sft2);
     assign   c_cs_edge = (~sr_cs_sync & sr_cs_sync_sft2);
+    assign   c_stopcnt = (sr_inst_cnt == 6'b100000)? 1'b1:
+                                                     1'b0;
+    assign   c_loadcnt = (sr_inst_cnt == 6'b100000)? 1'b1:
+                                                     1'b0;
+    assign   sr_inst_chk   = (c_loadcnt == 1'b1)? sr_inst_sht:
+                                                  1'b0;
+    
 //=================================================================
 //  Structural coding
 //=================================================================
-    // CS Shift Register
-    always_ff @(posedge SPI_CLK or negedge RESET_N or posedge c_cs_edge) begin
-        if(!RESET_N)
-            sr_inst_sht  <= 32'h0;
-        else if(c_cs_edge == 1'b1)  // CLR
-            sr_inst_sht  <= 32'h0;
-        else
-            sr_inst_sht[31:0]  <= {sr_inst_sht[30:0],SPI_MOSI};
-    end
     //  Instraction COUNTER
     always_ff @(posedge SPI_CLK or negedge RESET_N or posedge c_cs_edge) begin
         if(!RESET_N)
@@ -101,42 +124,24 @@ module ptmch_trg(
         else if(c_cs_edge == 1'b1)  // CLR
                 sr_inst_cnt  <= 6'b0;
         else
-            if(sr_inst_cnt == 6'b100001 )// STOP
+            if(c_stopcnt == 1'b1 )// STOP
                 sr_inst_cnt  <= sr_inst_cnt;
             else  // Count
                 sr_inst_cnt <= sr_inst_cnt + 1'b1;
     end
-    // Instraction chk(async)
-    always_ff @(posedge CLK160M or negedge RESET_N or posedge c_cs_edge) begin
-        if(!RESET_N | c_cs_edge == 1'b1)
-            ar_inst_chk  <= 32'h0;
+    // MOSI Shift Register
+    always_ff @(posedge SPI_CLK or negedge RESET_N or posedge c_cs_edge) begin
+        if(!RESET_N)
+            sr_inst_sht  <= 32'h0;
+        else if(c_cs_edge == 1'b1)  // CLR
+            sr_inst_sht  <= 32'h0;
         else
-            if(sr_inst_cnt == 6'b100000 ) // Load          
-                ar_inst_chk  <= sr_inst_sht;
-            else
-                ar_inst_chk  <= ar_inst_chk;
+            if(c_stopcnt == 1'b1)// STOP
+                sr_inst_sht <= sr_inst_sht;
+            else  // Count
+               sr_inst_sht[31:0]  <= {sr_inst_sht[30:0],SPI_MOSI};
     end
-    // Instraction chk(1d)
-    always_ff @(posedge CLK160M or negedge RESET_N or posedge c_cs_edge) begin
-        if(!RESET_N | c_cs_edge == 1'b1)
-            sr_inst_chk_1d  <= 32'h0;
-        else
-            sr_inst_chk_1d  <= ar_inst_chk;
-    end
-    // Instraction chk(2d)
-    always_ff @(posedge CLK160M or negedge RESET_N or posedge c_cs_edge) begin
-        if(!RESET_N | c_cs_edge == 1'b1)
-           sr_inst_chk_2d  <= 32'h0;
-        else
-            sr_inst_chk_2d  <= sr_inst_chk_1d;
-    end
-    // Instraction chk(3d)
-    always_ff @(posedge CLK160M or negedge RESET_N or posedge c_cs_edge) begin
-        if(!RESET_N | c_cs_edge == 1'b1)
-            sr_inst_chk_3d  <= 32'h0;
-        else
-            sr_inst_chk_3d  <= sr_inst_chk_2d;
-    end
+
     // Instracton Match pls(async)
     always_ff @(posedge CLK160M or negedge RESET_N or posedge c_cs_edge) begin
         if(!RESET_N | c_cs_edge == 1'b1)
@@ -199,26 +204,37 @@ module ptmch_trg(
             sr_cs_sync_sft2  <= sr_cs_sync_sft1;
     end
 
-    always @* begin
-        if((p_program_excute == sr_inst_chk_3d[31:24]) && (sr_inst_chk_3d[23:0]>=PRGEXCT_LOW_ADDR) && (sr_inst_chk_3d[23:0]<=PRGEXCT_HIGH_ADDR))
-            c_inst_mch <= 1'b1;
-        else begin
-            if((p_readstatus1 == sr_inst_chk_3d[31:24] )&&(sr_inst_chk_3d[23:0]>=RDSTAT_LOW_ADDR) && (sr_inst_chk_3d[23:0]<=RDSTAT_HIGH_ADDR))
-                c_inst_mch <= 1'b1;
-            else if((p_readstatus2 == sr_inst_chk_3d[31:24]) && (sr_inst_chk_3d[23:0]>=RDSTAT_LOW_ADDR) && (sr_inst_chk_3d[23:0]<=RDSTAT_HIGH_ADDR))
-                c_inst_mch <= 1'b1;
-            else if((p_128kb_blockerase == sr_inst_chk_3d[31:24]) && (sr_inst_chk_3d[23:0]>=BLKERS_LOW_ADDR) && (sr_inst_chk_3d[23:0]<=BLKERS_HIGH_ADDR))
-                c_inst_mch <= 1'b1;
-            else if((p_pagedata_read == sr_inst_chk_3d[31:24]) && (sr_inst_chk_3d[23:0]>=PDREAD_LOW_ADDR) && (sr_inst_chk_3d[23:0]<=PDREAD_HIGH_ADDR))
-                c_inst_mch <= 1'b1;
-            else if((p_writestatus1 == sr_inst_chk_3d[31:24]) && (sr_inst_chk_3d[23:0]>=WRSTAT_LOW_ADDR) && (sr_inst_chk_3d[23:0]<=WRSTAT_HIGH_ADDR))
-                c_inst_mch <= 1'b1;
-            else if((p_writestatus2 == sr_inst_chk_3d[31:24]) && (sr_inst_chk_3d[23:0]>=WRSTAT_LOW_ADDR) && (sr_inst_chk_3d[23:0]<=WRSTAT_HIGH_ADDR))
-                c_inst_mch <= 1'b1;
-            else
-                c_inst_mch <= 1'b0;
-        end
-    end
+    //program execute match
+    assign   c_prgex_ins        = (p_program_excute == sr_inst_chk[31:24]);
+    assign   c_prgex_addr_l     = (sr_inst_chk[23:0]>=PRGEXCT_LOW_ADDR);
+    assign   c_prgex_addr_h     = (sr_inst_chk[23:0]<=PRGEXCT_HIGH_ADDR);
+    assign   c_prgex_mch        = (c_prgex_ins & c_prgex_addr_l & c_prgex_addr_h);
+
+    //read status match
+    assign   c_rdsts_ins        = (p_readstatus1 == sr_inst_chk[31:24])|(p_readstatus2 == sr_inst_chk[31:24]);
+    assign   c_rdsts_addr_l     = (sr_inst_chk[23:0]>=RDSTAT_LOW_ADDR);
+    assign   c_rdsts_addr_h     = (sr_inst_chk[23:0]<=RDSTAT_HIGH_ADDR);
+    assign   c_rdsts_mch        = (c_rdsts_ins & c_rdsts_addr_l & c_rdsts_addr_h);
+
+    //128KB Block Erase match
+    assign   c_128kbbe_ins      = (p_128kb_blockerase == sr_inst_chk[31:24]);
+    assign   c_128kbbe_addr_l   = (sr_inst_chk[23:0]>=BLKERS_LOW_ADDR);
+    assign   c_128kbbe_addr_h   = (sr_inst_chk[23:0]<=BLKERS_HIGH_ADDR);
+    assign   c_128kbbe_mch      = (c_128kbbe_ins & c_128kbbe_addr_l & c_128kbbe_addr_h);
+
+    //page data read match
+    assign   c_pagedread_ins    = (p_pagedata_read == sr_inst_chk[31:24]);
+    assign   c_pagedread_addr_l = (sr_inst_chk[23:0]>=PDREAD_LOW_ADDR);
+    assign   c_pagedread_addr_h = (sr_inst_chk[23:0]<=PDREAD_HIGH_ADDR);
+    assign   c_pagedread_mch    = (c_pagedread_ins & c_pagedread_addr_l & c_pagedread_addr_h);
+
+    //write status match
+    assign   c_wrsts_ins        = (p_writestatus1 == sr_inst_chk[31:24])|(p_writestatus2 == sr_inst_chk[31:24]);
+    assign   c_wrsts_addr_l     = (sr_inst_chk[23:0]>=WRSTAT_LOW_ADDR);
+    assign   c_wrsts_addr_h     = (sr_inst_chk[23:0]<=WRSTAT_HIGH_ADDR);
+    assign   c_wrsts_mch        = (c_wrsts_ins & c_wrsts_addr_l & c_wrsts_addr_h);
+
+    assign   c_inst_mch        = (c_prgex_mch | c_rdsts_mch | c_128kbbe_mch | c_pagedread_mch | c_wrsts_mch);
 
     // TRG PLS time expander
     always @* begin
--- db/ip/sld60dec8c8/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ db/ip/sld60dec8c8/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 088f8a99a949da5802ea
+//   DESIGN_HASH   : 8cb30c9ebe73e366d70b
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h088f8a99a949da5802ea };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h8cb30c9ebe73e366d70b };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
